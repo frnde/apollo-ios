441d2042a2e13a32009ba0381b34f6a5ce6a5d44
Kay Butter <kay@kaybutter.de>
2022-12-20 11:42:30 +0100

    Remove platform check

--- Sources/ApolloCodegenLib/ApolloCodegen.swift
+++ Sources/ApolloCodegenLib/ApolloCodegen.swift
@@ -1,9 +1,6 @@
 import Foundation
 import OrderedCollections
 
-// Only available on macOS
-#if os(macOS)
-
 /// A class to facilitate running code generation
 public class ApolloCodegen {
 
@@ -600,5 +597,3 @@ public class ApolloCodegen {
   }
 
 }
-
-#endif
